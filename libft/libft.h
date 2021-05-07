2ddeb8852e9a471b60928f92f09db82e690818bc
Hoseong Yu <hoyu@c3r1s3.42seoul.kr>
2021-05-07 15:56:52 +0900

    libft/strlen libft/strlcpy

---
+++ libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoyu <hoyu@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/07 14:37:42 by hoyu              #+#    #+#             */
+/*   Updated: 2021/05/07 15:03:59 by hoyu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __LIBFT_H__
+# define __LIBFT_H__
+
+#include <stdlib.h>
+
+size_t ft_strlen(const char *str);
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
